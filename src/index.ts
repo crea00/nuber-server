b8d90eccc20c5790eebf8866b2649e115e7c1b46
crea00 <654321su@gmail.com>
2018-09-04 23:26:42 +0900

    #1.11 Creating a Virtual Environment on NodeJS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 // In alphabet sequence & module -> library
+import dotenv from "dotenv";
+dotenv.config();
+
 import { Options } from "graphql-yoga";
-import { createConnection } from 'typeorm';
+import { createConnection } from "typeorm";
 import app from "./app";
-import ConnectionOptions from "./ormConfig";
+import connectionOptions from "./ormConfig";
 
 const PORT: number | string = process.env.PORT || 4000;
 const PLAYGROUND_ENDPOINT: string = "/playground";
@@ -16,6 +19,8 @@ const appOptions: Options = {
 
 const handleAppStart = () => console.log(`Listening on port ${PORT}`);
 
-createConnection(ConnectionOptions).then(() => {
-  app.start(appOptions, handleAppStart);
-});
+createConnection(connectionOptions)
+  .then(() => {
+    app.start(appOptions, handleAppStart);
+  })
+  .catch(error => console.log(error));
